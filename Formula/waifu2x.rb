9c57130341a8d6c692e5359ca382d0d852148b81
Xie Yi <imxieyi@users.noreply.github.com>
2020-05-30 17:08:47 +0200

    v0.0.4

--- Formula/waifu2x.rb
+++ Formula/waifu2x.rb
@@ -4,9 +4,9 @@
 class Waifu2x < Formula
   desc "Waifu2x macOS Port"
   homepage "https://github.com/imxieyi/waifu2x-mac"
-  url "https://github.com/imxieyi/waifu2x-mac/releases/download/v0.0.3/waifu2x-mac-cli.zip"
-  version "0.0.3"
-  sha256 "f2b347274023a0ceedc52b44d3d92293845f567e959929210ba9ba239da43520"
+  url "https://github.com/imxieyi/waifu2x-mac/releases/download/v0.0.4/waifu2x-mac-cli.zip"
+  version "0.0.4"
+  sha256 "5254566897f8a939ad9421a5070a923d3515ca2fcbecd61e903955b2f7eceefb"
 
   def install
     system "mkdir", "-p", "#{prefix}/bin"
